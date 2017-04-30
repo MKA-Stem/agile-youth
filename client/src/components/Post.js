5e7dc70dbfc2977a7fb9d1c5c22fe2fcd0f8ebe5
William Goodall <wgoodall01@gmail.com>
2017-04-30 09:59:45 -0700

    Post styling

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,6 +1,7 @@
 import React from "react"
+import "./Post.css";
 
-const Post = ({expand=false, title, description}) => (<div>
+const Post = ({expand=false, title, description}) => (<div className="Post">
 	<h1>{title}</h1>
 	<p>{description}</p>
 	{expand?<div>
