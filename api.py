1c8c04221e077d8ebee34b941069c20c4615f038
Mark Apinis <mapinis2019@gmail.com>
2017-04-30 10:31:41 -0400

    some comments

--- api.py
+++ api.py
@@ -3,10 +3,12 @@ import os
 
 api = Blueprint("api", __name__, static_folder=None, template_folder=None)
 
+#API TEST
 @api.route("/test", methods=["GET"])
 def test():
     return jsonify({ "test":"yes" })
 
+#404
 @api.route("/<path:path>")
 def not_found(path):
     return jsonify({ "Not Found":path })
