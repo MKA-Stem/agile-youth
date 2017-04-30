743f84b46519c11f2a275f50983b4efb6fbb3cdd
William Goodall <wgoodall01@gmail.com>
2017-04-30 10:03:34 -0700

    Post styling

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,8 +1,9 @@
 import React from "react"
 import "./Post.css";
+import {Link} from "react-router-dom";
 
-const Post = ({expand=false, title, description}) => (<div className="Post">
-	<h1>{title}</h1>
+const Post = ({expand=false, title, description, id}) => (<div className="Post">
+	<h1><Link className="Post_link" to={`/post/${id}`}>{title}</Link></h1>
 	<p>{description}</p>
 	{expand?<div>
 		<div>Comments!</div>
