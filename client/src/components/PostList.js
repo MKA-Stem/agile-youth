f8f1ad53aab966ca3d900772b00f77e908c6d3dc
William Goodall <wgoodall01@gmail.com>
2017-04-30 08:51:13 -0700

    Load posts from server

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,18 +1,31 @@
 import React from "react";
 import Post from "components/Post";
 import {connect} from "react-redux";
+import {fetchPosts} from "state.js"
 
-const PostList = ({posts, postsLoading}) => (<div>
-	{postsLoading?<div>Loading posts... </div>:null}
-	{(posts && posts.length > 0)?
-		posts.map(p => <Post {...p} key={p.id}/>)
-		:<div>No posts.</div>
+class PostList extends React.Component{
+	componentDidMount(){
+		this.props.fetch() // Load posts.
 	}
-</div>)
 
+	render(){
+		const {postsLoading, posts} = this.props
+		return (<div>
+			{postsLoading?<div>Loading posts... </div>:null}
+			{(posts && posts.length > 0)?
+					posts.map(p => <Post {...p} key={p.id}/>)
+					:<div>No posts.</div>
+			}
+		</div>)
+	}
+}
 
 const mapStateToProps = (state) => ({
 	posts:state.posts
 })
 
-export default connect(mapStateToProps)(PostList);
+const mapDispatchToProps = (dispatch) => ({
+	fetch: () => dispatch(fetchPosts())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostList);
--- client/src/state.js
+++ client/src/state.js
@@ -1,13 +1,6 @@
 const defaultState = {
-	posts:[
-		{id: 1, title:"test", description:"Long text goes here!", score:1234},
-		{id: 2, title:"test", description:"Long text goes here!", score:1234},
-		{id: 3, title:"test", description:"Long text goes here!", score:1234},
-		{id: 4, title:"test", description:"Long text goes here!", score:1234},
-		{id: 5, title:"test", description:"Long text goes here!", score:1234},
-		{id: 6, title:"test", description:"Long text goes here!", score:1234},
-		{id: 7, title:"test", description:"Long text goes here!", score:1234},
-	]
+	posts:[]
+	postsLoading:false
 }
 
 const reducer = (state = defaultState, action) => {
