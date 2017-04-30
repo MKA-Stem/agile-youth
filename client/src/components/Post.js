e6536a3d135c37094199023cd6cf4280d7cfb657
William Goodall <wgoodall01@gmail.com>
2017-04-30 10:47:53 -0700

    Comments

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -2,11 +2,12 @@ import React from "react"
 import "./Post.css";
 import {Link} from "react-router-dom";
 
-const Post = ({expand=false, title, description, id}) => (<div className="Post">
+const Post = ({expand=false, title, description, id, comments}) => (<div className="Post">
 	<h1><Link className="Post_link" to={`/post/${id}`}>{title}</Link></h1>
 	<p>{description}</p>
+	<div className="Post_comments-header">Comments:</div>
 	{expand?<div>
-		<div>Comments!</div>
+		{comments.map((c, i) => <div key={i} className="Post_comment">{c.comment}</div>)}
 	</div>:<div/>}
 </div>)
 
