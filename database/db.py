881b9e6cbeed9611a94d75ff19ecf1fb94ceb406
AaronCGoidel <agoidel2019@mka.org>
2017-04-30 10:29:24 -0400

    added database code

---
+++ database/db.py
@@ -0,0 +1,40 @@
+# /usr/bin/env python3
+
+# This has the rethink connection stuff.
+
+import rethinkdb as r
+import os
+
+DB_NAME = "SGO"
+
+
+def connect():
+    """Returns a rethink connection"""
+
+    RDB_HOST = os.environ.get('RDB_HOST') or 'localhost'
+    RDB_PORT = os.environ.get('RDB_PORT') or 28015
+    return r.connect(host=RDB_HOST, port=RDB_PORT, db=DB_NAME)
+
+
+def setup():
+    cn = connect()
+
+    def maketable(name):
+        if name not in r.table_list().run(cn):
+            r.table_create(name).run(cn)
+
+    def makeindex(table, index):
+        if index not in r.table(table).index_list().run(cn):
+            r.table(table).index_create(index).run(cn)
+
+    if DB_NAME not in r.db_list().run(cn):
+        r.db_create(DB_NAME).run(cn)
+
+    maketable("users")
+    makeindex("users", "username")
+
+
+r = r
+
+# Setup db if run
+if __name__ == "__main__": setup()
