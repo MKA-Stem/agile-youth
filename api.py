bf970eec98aaf09df04d8a8659ea129725a036b5
AaronCGoidel <agoidel2019@mka.org>
2017-04-30 10:48:13 -0400

    added post endpoint

--- api.py
+++ api.py
@@ -8,6 +8,14 @@ api = Blueprint("api", __name__, static_folder=None, template_folder=None)
 def test():
     return jsonify({ "test":"yes" })
 
+#return posts from database
+@api.route("/posts")
+def post():
+    return jsonify(
+        {"posts":[{"title":"No More Vegetarian Food", "desc":"we need normal people food!","score":1000, "id":69},
+        {"title":"Vote Cole", "desc":"Cole for prez!","score":345, "id":420}]} # dummy values
+    )
+
 #404
 @api.route("/<path:path>")
 def not_found(path):
@@ -18,3 +26,5 @@ class ERRORS():
     BAD_CREDS = ('{"error":"Bad credentials"}', 400)
     BAD_REQ = ('{"error":"Bad request"})', 400)
     BAD_AUTH = ('{"error":"Bad authentication"}', 400)
+
+
