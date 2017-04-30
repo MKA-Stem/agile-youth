a8bb988d517e2207aa2ada4fb759b554600ae3cb
AaronCGoidel <agoidel2019@mka.org>
2017-04-30 12:54:45 -0400

    tweaked weight code

--- server_utils/ranking/weightCalc.py
+++ server_utils/ranking/weightCalc.py
@@ -1,15 +1,12 @@
-import time
-from math import log
-
-currentTimeMilli = lambda: int(round(time.time() * 1000))
+from math import log10
 
 HALF_LIFE = 45000 # half-life of a post's score is 12.5 hours
 
 
-def weight(likes, dislikes, postTime):
-    seconds = (currentTimeMilli() - postTime) / 1000
+def weight(likes, dislikes, postTime, startTime):
+    seconds = (startTime - postTime) / 1000
     likeScore = likes - dislikes
-    order = log(max(likeScore, 1), 10)
+    order = log10(max(abs(likeScore), 1))
     sign = 1 if likeScore > 0 else 0 if likeScore == 0 else -1
 
     return round(sign * order + seconds / HALF_LIFE, 7)
