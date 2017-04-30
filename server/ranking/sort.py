6444cde0c6e3ca5afaefa39f3b0641c65da45100
AaronCGoidel <agoidel2019@mka.org>
2017-04-30 11:33:02 -0400

    fixed time import

---
+++ server/ranking/sort.py
@@ -0,0 +1,24 @@
+def heapSort(numList):
+    """Heap Sort Algorithm"""
+    for start in range((len(numList) - 2) / 2, -1, -1):
+        siftdown(numList, start, len(numList) - 1)
+
+    for end in range(len(numList) - 1, 0, -1):
+        numList[end], numList[0] = numList[0], numList[end]
+        siftdown(numList, 0, end - 1)
+    return numList
+
+
+def siftdown(numList, start, end):
+    """Requisite for Heap Sort"""
+    root = start
+    while True:
+        child = root * 2 + 1
+        if child > end: break
+        if child + 1 <= end and numList[child] < numList[child + 1]:
+            child += 1
+        if numList[root] < numList[child]:
+            numList[root], numList[child] = numList[child], numList[root]
+            root = child
+        else:
+            break
